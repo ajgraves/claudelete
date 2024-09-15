d0866720522e378be3f8b9a0b179b17950354cb2
Aaron Graves <ajgraves@ajgraves.com>
2024-09-15 11:47:34 -0500

    More time between deletes to be more limit friendly

--- claudelete.py
+++ claudelete.py
@@ -338,8 +338,8 @@ async def process_channel(guild, channel, delete_after):
                         delete_count += 1
                         await progress_queue.put(1)
                         
-                        # Add a random delay between 0.25 and 1 seconds
-                        await asyncio.sleep(random.uniform(0.25, 1))
+                        # Add a random delay between 0. and 1 seconds
+                        await asyncio.sleep(random.uniform(0.5, 1))
                         
                     except NotFound:
                         print(f"Message not found in channel {channel.id}, guild {guild.id}")
